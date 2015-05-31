267f28f453a53fa95ef65e933cd1b5b236bacf2e
Sebastian Andersson <bofh@diegeekdie.com>
2015-05-31 20:06:02 +0200

    Added a reader for Sjöfartsverkets ViVa data service.

---
+++ vivaReader.js
@@ -0,0 +1,40 @@
+//
+// # vivaReader
+//
+// A module to read weather-data (wind direction and speed).
+//
+
+exports.pollServer = function(cfg, cb) {
+    var soap = require("soap");
+
+    soap.createClient(cfg.url, function(err, client) {
+	if(err == null) {
+	    client.GetViVaData(cfg.args, function(err, result) {
+		if(err == null) {
+		    result = result.GetViVaDataResult;
+		    if(result.Felmeddelande != null) {
+			console.log("Got error from GetViVaData call:" + result.Felmeddelande);
+		    } else {
+			var wd = {};
+			wd.name = result.PlatsNamn;
+			result.ViVaData.forEach(function(o) {
+			    o = o.attributes;
+			    if(o.Typ == 'Riktning') {
+				wd.dir = o.Varde;
+			    } else if(o.Typ == 'Medelvind') {
+				wd.avgSpeed = o.Varde;
+			    } else if(o.Typ == 'Bywind') {
+				wd.maxSpeed = o.Varde;
+			    }
+			});
+			cb(wd);
+		    }
+		} else {
+		    console.log("Got error: " + err);
+		};
+	    });
+	} else {
+	    console.log("Got error: " + err);
+	};
+    });
+}
