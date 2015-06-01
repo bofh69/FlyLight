ab4e8bc08b0633a03e4aa46e999676f82055541e
Sebastian Andersson <bofh@diegeekdie.com>
2015-06-01 18:13:58 +0200

    Made a little app to list the available ViVa stations.

---
+++ listVivaStations.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var soap = require("soap");
+
+var url = 'http://161.54.134.239/vivadata.asmx?WSDL';
+
+// https://www.google.se/maps/@57.9525834,11.5709113,19z?hl=sv
+
+var mapUrl = 'https://www.google.se/maps/@';
+
+soap.createClient(url, function(err, client) {
+    if(err == null) {
+	client.GetViVaPoints(function(err, result) {
+	    // Sort from south to north:
+	    result.GetViVaPointsResult.ViVaPoint.sort(function(a, b) {
+		var aLatitude = a.attributes.Latitude.replace(',', '.');
+		var bLatitude = b.attributes.Latitude.replace(',', '.');
+		return aLatitude - bLatitude;
+	    }).forEach(function(o) {
+		o = o.attributes;
+		o.Latitude = o.Latitude.replace(',', '.');
+		o.Longitude = o.Longitude.replace(',', '.');
+		// Print the id, name and an URL to google maps:
+		console.log('Id:', o.PlatsId, 'Namn:', o.Platsnamn, mapUrl + o.Latitude + ',' + o.Longitude + ',13z');
+	    });
+	});
+    } else {
+	console.log("Got error: " + err);
+    }
+});
