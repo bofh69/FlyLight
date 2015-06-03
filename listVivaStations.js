4d94116e94ee2fe06d8ade8e06576efb0c399bc6
Sebastian Andersson <bofh69@gmail.com>
2015-06-03 07:02:51 +0000

    Added some better error handling.

--- listVivaStations.js
+++ listVivaStations.js
@@ -10,21 +10,25 @@ var mapUrl = 'https://www.google.se/maps/@';
 
 soap.createClient(url, function(err, client) {
     if(err == null) {
-	client.GetViVaPoints(function(err, result) {
-	    // Sort from south to north:
-	    result.GetViVaPointsResult.ViVaPoint.sort(function(a, b) {
-		var aLatitude = a.attributes.Latitude.replace(',', '.');
-		var bLatitude = b.attributes.Latitude.replace(',', '.');
-		return aLatitude - bLatitude;
-	    }).forEach(function(o) {
-		o = o.attributes;
-		o.Latitude = o.Latitude.replace(',', '.');
-		o.Longitude = o.Longitude.replace(',', '.');
-		// Print the id, name and an URL to google maps:
-		console.log('Id:', o.PlatsId, 'Namn:', o.Platsnamn, mapUrl + o.Latitude + ',' + o.Longitude + ',13z');
-	    });
-	});
+	    client.GetViVaPoints(function(err, result) {
+		    // Sort from south to north:
+		    if(err == null) {
+			    result.GetViVaPointsResult.ViVaPoint.sort(function(a, b) {
+				var aLatitude = a.attributes.Latitude.replace(',', '.');
+				var bLatitude = b.attributes.Latitude.replace(',', '.');
+				return aLatitude - bLatitude;
+			    }).forEach(function(o) {
+				o = o.attributes;
+				o.Latitude = o.Latitude.replace(',', '.');
+				o.Longitude = o.Longitude.replace(',', '.');
+				// Print the id, name and an URL to google maps:
+				console.log('Id:', o.PlatsId, 'Namn:', o.Platsnamn, mapUrl + o.Latitude + ',' + o.Longitude + ',13z');
+			    });
+		    } else {
+		    	console.log("Got error response from server: " + err)
+		    }
+		});
     } else {
-	console.log("Got error: " + err);
+	    console.log("Got error: " + err);
     }
 });
