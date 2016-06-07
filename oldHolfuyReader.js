66ce95be78d27ee1bc9622402423bbd3358346ce
Sebastian Andersson <sebastian@bittr.nu>
2016-06-07 21:30:51 +0200

    Added a reader for the old Holfuy format. Added it to the config.json file.

---
+++ oldHolfuyReader.js
@@ -0,0 +1,45 @@
+//
+// # oldHolfuyReader
+//
+// A module to read weather-data (wind direction and speed).
+//
+
+var http = require("http");
+
+function convertKnotToMps(knot)
+{
+  return knot/1.9438444924574;
+}
+
+function parseClientRaw(str) {
+  var result = {};
+
+  var splitBody = str.split(' ');
+  result.dir = splitBody[3];
+  result.avgSpeed = convertKnotToMps(splitBody[1]);
+  result.maxSpeed = convertKnotToMps(splitBody[2]);
+  result.name = splitBody[32].replace(/_/g, " ");
+
+  return result;
+}
+
+exports.pollServer = function(cfg, cb) {
+  http.get(cfg.url, function(res) {
+    if (res.statusCode == 200) {
+	try {
+	    var body = "";
+	    res.on('data', function(chunk) {
+		body += chunk;
+	    });
+	    res.on('end', function() {
+		var wd = parseClientRaw(body);
+		cb(wd);
+	    });
+	} catch(ex) {
+	    console.log("Got error while parsing weather station '" + cfg.url + "': " + ex);
+	};
+    }
+  }).on('error', function(e) {
+      console.log("Got error while reading weather station '" + cfg.url + "': " + e.message);
+  });;
+}
